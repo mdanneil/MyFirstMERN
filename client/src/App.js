a87d0fdd03c8961ca0c00147c5f5444c5d4ec5ff
ddewe <ddewee92@gmail.com>
2021-12-21 13:28:07 +0100

    some styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import useStyles from './styles'
 
 const memories =
   'https://raw.githubusercontent.com/adrianhajdin/project_mern_memories/master/client/src/images/memories.png?token=AF56X74XONEUGZ4FD2FUIA27UURPI'
 
 const App = () => {
+  const classes = useStyles()
   return (
     <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
-          <img src={memories} alt="memories" height="60" />
+          <img
+            className={classes.image}
+            src={memories}
+            alt="memories"
+            height="60"
+          />
         </Typography>
       </AppBar>
       <Grow in>
