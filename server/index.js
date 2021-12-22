60ca343c64230a9553c711bf7fbdb563a460b568
ddewe <ddewee92@gmail.com>
2021-12-22 18:47:19 +0100

    added action and fixed a cors error from the server

--- server/index.js
+++ server/index.js
@@ -6,12 +6,11 @@ import postRoutes from './routes/posts.js'
 
 const app = express()
 
-app.use('/posts', postRoutes)
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
+app.use('/posts', postRoutes)
 const CONNECTION_URL =
   'mongodb+srv://TestUser:TestPassword123@cluster0.idpht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
