d41194e7ff1bfb26925686f45f737a35cc532d1a
ddewe <ddewee92@gmail.com>
2021-12-22 20:52:18 +0100

    can successfully submit a post to the db

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -3,7 +3,7 @@ const reducer = (posts = [], action) => {
     case 'FETCH_ALL':
       return action.payload
     case 'CREATE_POST':
-      return posts
+      return [...posts, action.payload]
     default:
       return posts
   }
